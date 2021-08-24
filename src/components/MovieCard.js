ef008b38571659443980da7f6b0a02dcb26fd84f
unknown <houssemhosni2018@gmail.com>
2021-08-24 02:06:58 +0200

    first commit

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,10 @@
+import {Link} from 'react-router-dom';
 function MovieCard(props) {
     return (
-            <div className="MovieCard">
-                <img src={props.posterUrl} alt="avengers"></img>  
+      <div className="MovieCard" >
+      <Link to={"/" + props.id}>
+        <img src={props.posterUrl} alt="avengers"></img>
+      </Link>
                 <div className="movieInfo">
                 <h3>{props.title}</h3>
                 <span>{props.rate}</span>
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,9 +4,9 @@ function MovieList(props) {
     const movies = props.movies;
     const movie = movies.map((x) => {
         return (
-                <>
-                    <MovieCard title={x.title} description={x.description} posterUrl={x.posterUrl} rate={x.rate} />
-                </>
+                <div key={x.id}>
+                    <MovieCard title={x.title} description={x.description} posterUrl={x.posterUrl} rate={x.rate} id={x.id} trailerLink={x.trailerLink} />
+                </div>
         );
     })
     return (
